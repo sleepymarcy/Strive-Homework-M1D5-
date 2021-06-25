7833f5678e2a997496ad3f6c4866bd636ed64c48
Martyna <martyna.sowinska@hotmail.com>
2021-06-25 16:36:14 +0200

    Exercise 5 - solved

--- exercises.js
+++ exercises.js
@@ -94,14 +94,23 @@ Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
+function strivify(string){
+    if (string.startsWith("Strive")){
+        return string
+    }
+    else
+    return "Strive" + " " + string
+}
+
+// console.log(strivify("Strive is the best!"))
+// console.log(strivify("is the best!"))
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
 
-/* WRITE YOUR CODE HERE */
+
 
 /* EXERCISE 7
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
